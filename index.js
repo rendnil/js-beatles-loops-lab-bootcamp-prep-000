d0237ecfc708d3153b08e9034f56bbb06a805060
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-03-08 02:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ function theBeatlesPlay(musicians, instruments){
   }
   
 function johnLennonFacts(facts) {
-  
-  
-  
+  i = 0
+  while (i<facts.length){
+    
+    facts[i]= facts[i]+"!"
+    i++
+    
+    
+  }
+  return facts
 }
